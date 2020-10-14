1fc75b73f803fc2628ca7e002f2b2ab1e499f920
liuxiong.lx <liuxiong.lx@alibaba-inc.com>
2020-10-14 17:10:27 +0800

    chore: ci

--- scripts/upload.ts
+++ scripts/upload.ts
@@ -1,26 +1,24 @@
 import * as oss from 'ali-oss';
 import * as path from 'path';
-import injectComponents from './injectComponents';
 
 const bucket = 'iceworks';
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const accessKeySecret = process.env.ACCESS_KEY_SECRET;
 
-injectComponents().then(() => {
-  oss({
-    bucket,
-    endpoint: 'oss-cn-hangzhou.aliyuncs.com',
-    accessKeyId,
-    accessKeySecret,
-    timeout: '120s',
-  })
-    // @ts-ignore
-    .put(
-      path.join('assets', 'materials', 'rax-materials.json'),
-      path.resolve(__dirname, '../build/materials.json')
-    ).then(result => {
-      console.log('materials.json upload success', result);
-    });
-}).catch((e) => {
-  console.error(e);
-});
+oss({
+  bucket,
+  endpoint: 'oss-cn-hangzhou.aliyuncs.com',
+  accessKeyId,
+  accessKeySecret,
+  timeout: '120s',
+})
+  // @ts-ignore
+  .put(
+    path.join('assets', 'materials', 'antd-materials.json'),
+    path.resolve(__dirname, '../build/materials.json')
+  ).then(result => {
+    console.log('materials.json upload success', result);
+  }).catch((e) => {
+    console.error(e);
+    process.exit(-1);
+  });
